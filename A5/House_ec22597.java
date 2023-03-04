8f8f7663bf5f50108c51916de2edf38c3d0c3a57
Theo Georgiou <75097082+Theo03iou@users.noreply.github.com>
2023-03-04 18:31:44 +0000

    Add files via upload

---
+++ A5/House_ec22597.java
@@ -0,0 +1,10 @@
+public class House_ec22597  extends House {
+    public Direction visit(Visitor v, Direction d) {
+
+        if ()
+
+        return d;
+    }
+} 
+    
+
---
+++ A5/Room_ec22597.java
@@ -0,0 +1,3 @@
+public class Room_ec22597 {
+    
+}
